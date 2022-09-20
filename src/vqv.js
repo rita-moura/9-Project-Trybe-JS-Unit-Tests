f10a2fed9f4d34a8c49e1ee790ac3066fcb3a6a1
Rita Moura <ritamoura520@gmail.com>
2022-09-20 17:49:26 -0300

    Requisito 3 feito

--- src/vqv.js
+++ src/vqv.js
@@ -18,6 +18,17 @@
   Caso a função seja chamada sem nenhum parâmetro, o valor undefined deve ser retornado.
 */
 
-const vqv = () => {};
+const vqv = (string, number) => {
+  const sentence = 'Oi, meu nome é';
+  const sentencetwo = 'trabalho na Trybe e mando muito em programação!';
+  let name = string;
+  let age = number;
+  let concat = `${sentence} ${name}!\nTenho ${age} anos,\n${sentencetwo}\n#VQV!`;
+  if (string === undefined || number === undefined) {
+    return undefined;
+  }
+  return concat;
+};
+/* console.log(vqv('Tonico', 30)); */
 
 module.exports = vqv;
